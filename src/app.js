ce4eb12c6deedd34a87df3e380510135c570c1ab
Jonathan Gutiérrez <jonathangutierrezperez@gmail.com>
2023-02-11 13:02:53 +0000

    sincss

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function onload() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excusa = "";
+
+  excusa +=
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 4)] +
+    " " +
+    when[Math.floor(Math.random() * 4)];
+  document.getElementById("excuse").innerHTML = excusa;
 };
